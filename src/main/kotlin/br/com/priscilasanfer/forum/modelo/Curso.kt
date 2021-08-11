6bd15e2caae8af007fffba24fb89cc66755ac5c2
Priscila Ferreira <priscilasanfer@gmail.com>
2021-08-11 17:10:55 -0300

    Classes de domínio

---
+++ src/main/kotlin/br/com/priscilasanfer/forum/modelo/Curso.kt
@@ -0,0 +1,7 @@
+package br.com.priscilasanfer.forum.modelo
+
+data class Curso(
+        val id: Long? = null,
+        val nome: String,
+        val categoria: String
+)
---
+++ src/main/kotlin/br/com/priscilasanfer/forum/modelo/StatusTopico.kt
@@ -0,0 +1,9 @@
+package br.com.priscilasanfer.forum.modelo
+
+enum class StatusTopico {
+    NAO_RESPONDIDO,
+    NAO_SOLUCIONADO,
+    SOLUCIONADO,
+    FECHADO
+
+}
